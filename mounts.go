ebea5d1b45ca8f42c0c22c7edff52bdc23f87ccd
Christian Muehlhaeuser <muesli@gmail.com>
2020-09-21 06:06:20 +0200

    Remove commented-out code

--- mounts.go
+++ mounts.go
@@ -77,26 +77,7 @@ func mounts() ([]Mount, error) {
 			if len(match) > 0 && len(match[0]) == 3 {
 				d.Device = filepath.Join("/dev", match[0][1], match[0][2])
 			}
-
-			/*
-				devpath, err := filepath.EvalSymlinks(common.HostDev(strings.Replace(d.Device, "/dev", "", -1)))
-				if err == nil {
-					d.Device = devpath
-				}
-			*/
 		}
-
-		// /dev/root is not the real device name
-		// so we get the real device name from its major/minor number
-		/*
-			if d.Device == "/dev/root" {
-				devpath, err := os.Readlink(common.HostSys("/dev/block/" + blockDeviceID))
-				if err != nil {
-					return nil, err
-				}
-				d.Device = strings.Replace(d.Device, "root", filepath.Base(devpath), 1)
-			}
-		*/
 
 		ret = append(ret, d)
 	}
@@ -136,40 +117,3 @@ func StringsHas(target []string, src string) bool {
 	}
 	return false
 }
-
-/*
-	bsize := stat.Bsize
-
-	ret := &UsageStat{
-		Path:        unescapeFstab(path),
-		Fstype:      getFsType(stat),
-		Total:       (uint64(stat.Blocks) * uint64(bsize)),
-		Free:        (uint64(stat.Bavail) * uint64(bsize)),
-		InodesTotal: (uint64(stat.Files)),
-		InodesFree:  (uint64(stat.Ffree)),
-	}
-
-	// if could not get InodesTotal, return empty
-	if ret.InodesTotal < ret.InodesFree {
-		return ret, nil
-	}
-
-	ret.InodesUsed = (ret.InodesTotal - ret.InodesFree)
-	ret.Used = (uint64(stat.Blocks) - uint64(stat.Bfree)) * uint64(bsize)
-
-	if ret.InodesTotal == 0 {
-		ret.InodesUsedPercent = 0
-	} else {
-		ret.InodesUsedPercent = (float64(ret.InodesUsed) / float64(ret.InodesTotal)) * 100.0
-	}
-
-	if (ret.Used + ret.Free) == 0 {
-		ret.UsedPercent = 0
-	} else {
-		// We don't use ret.Total to calculate percent.
-		// see https://github.com/shirou/gopsutil/issues/562
-		ret.UsedPercent = (float64(ret.Used) / float64(ret.Used+ret.Free)) * 100.0
-	}
-
-	return ret, nil
-*/
