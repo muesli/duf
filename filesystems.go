577c17fedd06a80aa3d4a85d9b08f102b53ea254
Christian Muehlhaeuser <muesli@gmail.com>
2020-11-01 12:15:07 +0100

    Add support for path arguments to filter output

--- filesystems.go
+++ filesystems.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func findMounts(mounts []Mount, path string) ([]Mount, error) {
+	var err error
+	path, err = filepath.Abs(path)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var m []Mount
+	for _, v := range mounts {
+		if path == v.Device {
+			return []Mount{v}, nil
+		}
+
+		if strings.HasPrefix(path, v.Mountpoint) {
+			var nm []Mount
+
+			// keep all entries that are as close or closer to the target
+			for _, mv := range m {
+				if len(mv.Mountpoint) >= len(v.Mountpoint) {
+					nm = append(nm, mv)
+				}
+			}
+			m = nm
+
+			// add entry only if we didn't already find something closer
+			if len(nm) == 0 || len(v.Mountpoint) >= len(nm[0].Mountpoint) {
+				m = append(m, v)
+			}
+		}
+	}
+
+	return m, nil
+}
+
 func deviceType(m Mount) string {
 	if isNetworkFs(m) {
 		return networkDevice
