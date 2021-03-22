297032b4a25ec6f03bc90968011647352cbc723b
Christian Muehlhaeuser <muesli@gmail.com>
2021-03-22 18:33:02 +0100

    gofmt style.go

--- style.go
+++ style.go
@@ -4,9 +4,9 @@ import "github.com/mattn/go-runewidth"
 
 func defaultStyleName() string {
 	/*
-	Due to a bug in github.com/mattn/go-runewidth v0.0.9, the width of unicode rune(such as '╭') could not be correctly
-	calculated.	Degrade to ascii to prevent broken table structure. Remove this once the bug is fixed.
-	 */
+		Due to a bug in github.com/mattn/go-runewidth v0.0.9, the width of unicode rune(such as '╭') could not be correctly
+		calculated.	Degrade to ascii to prevent broken table structure. Remove this once the bug is fixed.
+	*/
 	if runewidth.RuneWidth('╭') > 1 {
 		return "ascii"
 	}
