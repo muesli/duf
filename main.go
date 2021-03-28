901ff54ea0a297924a8a2a4a80e2c8bc34df85fe
Adrien Kara <adrien@iglou.eu>
2021-03-28 19:46:44 +0200

    Moving `wildcard` repos without full Minio pkg
    Signed-off-by: Adrien Kara <adrien@iglou.eu>

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 
 	wildcard "git.iglou.eu/Imported/go-wilcard"
 	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/minio/minio/pkg/wildcard"
 	"github.com/muesli/termenv"
 	"golang.org/x/crypto/ssh/terminal"
 )
