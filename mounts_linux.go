c5f4096e44acfdc22cc6fd5105e1cd61a3759771
Adrien Kara <adrien@iglou.eu>
2022-02-06 04:28:50 +0100

    Commenting "deadcode"
    Signed-off-by: Adrien Kara <adrien@iglou.eu>

--- mounts_linux.go
+++ mounts_linux.go
@@ -14,32 +14,32 @@ import (
 )
 
 const (
-	// a line of self/mountinfo has the following structure:
+	// A line of self/mountinfo has the following structure:
 	// 36  35  98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue
 	// (1) (2) (3)   (4)   (5)      (6)      (7)   (8) (9)   (10)         (11)
 	//
-	// (1) mount ID: unique identifier of the mount (may be reused after umount)
-	mountinfoMountID = iota + 1
-	// (2) parent ID: ID of parent (or of self for the top of the mount tree)
-	mountinfoParentID
-	// (3) major:minor: value of st_dev for files on filesystem
-	mountinfoMajorMinor
-	// (4) root: root of the mount within the filesystem
-	mountinfoRoot
-	// (5) mount point: mount point relative to the process's root
-	mountinfoMountPoint
-	// mountinfoMountOpts (6) mount options: per mount options
-	mountinfoMountOpts
-	// (7) optional fields
-	mountinfoOptionalFields
-	// (8) separator between optional fields
-	mountinfoSeparator
-	// (9) filesystem type: name of filesystem of the form
-	mountinfoFsType
-	// (10) mount source: filesystem specific information or "none"
-	mountinfoMountSource
-	// (11) super options: per super block options
-	mountinfoSuperOptions
+	// (1) mount ID: unique identifier of the mount (may be reused after umount).
+	//mountinfoMountID = 1
+	// (2) parent ID: ID of parent (or of self for the top of the mount tree).
+	//mountinfoParentID = 2
+	// (3) major:minor: value of st_dev for files on filesystem.
+	//mountinfoMajorMinor = 3
+	// (4) root: root of the mount within the filesystem.
+	//mountinfoRoot = 4
+	// (5) mount point: mount point relative to the process's root.
+	mountinfoMountPoint = 5
+	// (6) mount options: per mount options.
+	mountinfoMountOpts = 6
+	// (7) optional fields: zero or more fields terminated by "-".
+	mountinfoOptionalFields = 7
+	// (8) separator between optional fields.
+	//mountinfoSeparator = 8
+	// (9) filesystem type: name of filesystem of the form.
+	mountinfoFsType = 9
+	// (10) mount source: filesystem specific information or "none".
+	mountinfoMountSource = 10
+	// (11) super options: per super block options.
+	//mountinfoSuperOptions = 11
 )
 
 func (m *Mount) Stat() unix.Statfs_t {
